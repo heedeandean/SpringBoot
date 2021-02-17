376e21196bff9920e53e784e1a2ad7301aa3576c
heedeandean <7942ahj@hanmail.net>
2021-02-18 01:23:09 +0900

    2021-02-18

---
+++ MyBatis_Param/src/main/java/com/study/springboot/MyBatisParamApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisParamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisParamApplication.class, args);
+	}
+
+}
