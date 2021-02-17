f3dd6ad20f1339a06ae076dfaf85ffc87eac87a0
heedeandean <7942ahj@hanmail.net>
2021-02-18 06:02:41 +0900

    2021-02-18

---
+++ TransactionManager/src/test/java/com/study/springboot/TransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
