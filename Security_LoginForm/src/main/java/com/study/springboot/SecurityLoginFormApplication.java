eef4315e6148c9816ab87dadb5c3cdbe03b441a5
heedeandean <7942ahj@hanmail.net>
2021-02-19 00:10:27 +0900

    2021-02-19

---
+++ Security_LoginForm/src/main/java/com/study/springboot/SecurityLoginFormApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLoginFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLoginFormApplication.class, args);
+	}
+
+}
---
+++ Security_StatusCheck/src/main/java/com/study/springboot/SecurityStatusCheckApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStatusCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityStatusCheckApplication.class, args);
+	}
+
+}
