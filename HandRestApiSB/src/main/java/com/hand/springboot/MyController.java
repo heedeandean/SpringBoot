f4eafce5938270385278fa69647a45895a5a3254
heedeandean <7942ahj@hanmail.net>
2021-03-04 22:14:27 +0900

    2021-03-04

--- HandRestApiSB/src/main/java/com/hand/springboot/MyController.java
+++ HandRestApiSB/src/main/java/com/hand/springboot/MyController.java
@@ -20,26 +20,23 @@ public class MyController {
 	@PostConstruct
 	public void init() {
 		userMap = new HashMap<String, Member>();
-		userMap.put("hello", new Member("hello", "", "", ""));
 	}
 
 	
 	// http://localhost:3000/user?loginId=hello&password=world&nickname=handstudio
 	@PostMapping("/user")
-	public @ResponseBody String postMember(@RequestParam("loginId") String loginId
-					     , @RequestParam("password") String password
-					     , @RequestParam("nickname") String nickname) 
-	{
-		JSONObject obj = new JSONObject();
+	public @ResponseBody String postMember(@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password, @RequestParam("nickname") String nickname) {
 		
-		try {
-			Member member = userMap.get(loginId);
-			member.setPassword(password);
-			member.setNickname(nickname);
+		JSONObject obj = new JSONObject();
 
+		try {
+			Member member = new Member(loginId, password, nickname, "");
+			userMap.put(loginId, member);
+
+			System.out.println("USER CREATE! : " + userMap.get(loginId));
 			obj.put("success", true);
-			System.out.println(userMap);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			obj.put("success", false);
@@ -47,27 +44,34 @@ public class MyController {
 
 		return obj.toJSONString();
 	}
-
 	
+
 	// http://localhost:3000/login?loginId=hello&password=world
 	@PostMapping("/login")
-	public @ResponseBody String loginMember(@RequestParam("loginId") String loginId
-					      , @RequestParam("password") String password) 
-	{
+	public @ResponseBody String loginMember(@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password) {
+		
 		JSONObject obj = new JSONObject();
+		
 		try {
 			Member member = userMap.get(loginId);
-			member.setToken("123456");
 
-			obj.put("success", true);
-			obj.put("token", member.getToken());
+			if (member.getPassword().equals(password)) {
+				
+				member.setToken("123456");
 
-			System.out.println(userMap);
-
+				obj.put("success", true);
+				obj.put("token", member.getToken());
+			} 
+			else {
+				obj.put("success", false);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			obj.put("success", false);
 		}
+
+		System.out.println(userMap);
 
 		return obj.toJSONString();
 	}
@@ -77,16 +81,22 @@ public class MyController {
 	@GetMapping("/user")
 	public @ResponseBody String getMember(@RequestParam("token") String token) {
 		JSONObject obj = new JSONObject();
-		String loginId = null;
-
-		for (Map.Entry<String, Member> entry : userMap.entrySet()) {
-			if (entry.getValue().getToken().equals(token)) {
-				loginId = entry.getKey();
+		try {
+			String loginId = null;
+	
+			for (Map.Entry<String, Member> entry : userMap.entrySet()) {
+				if (entry.getValue().getToken().equals(token)) {
+					loginId = entry.getKey();
+				}
 			}
+			
+			Member member = userMap.get(loginId);
+			obj.put("loginId", member.getLoginId());
+			obj.put("nickname", member.getNickname());
+		} catch (Exception e) {
+			e.printStackTrace();
+			obj.put("success", false);
 		}
-		Member member = userMap.get(loginId);
-		obj.put("loginId", member.getLoginId());
-		obj.put("nickname", member.getNickname());
 
 		return obj.toJSONString();
 	}
